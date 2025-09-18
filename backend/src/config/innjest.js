df38b3bb3b437636d42ee9338079d0a53fff2578
jagannathcoding <jagannathdebgunin@gmail.com>
2025-09-18 17:37:29 +0530

    auth implemented

--- backend/src/config/innjest.js
+++ backend/src/config/innjest.js
@@ -7,16 +7,35 @@ export const inngest = new Inngest({ id: "slack-clone" });
 
 
 const syncUser=inngest.createFunction(
-    {id:"sync-user"},
-    {event:"clerk/user.created"},
+    {id:"sync-user"},///name for this function
+    {event:"clerk/user.created"},///kon event ei function ke trigger korbe
     async({event})=>{
         await connectDB();
 
         const{id,email_addresses,first_name,last_name,image_url}=event.data;
-    }
-)
 
+        const newUser={
+            clerkId:id,
+            email:email_addresses[0]?.email_address,
+            name:`${first_name||""} ${last_name||""}`,
+            image:image_url,
+
+        }
+        await User.create(newUser);
+    }
+);
+
+
+const deleteUserFromDB=inngest.createFunction(
+    {id:"delete-user-from-db"},
+    {event:"clerk/user.deleted"},///this is comong from clerk
+    async({event})=>{
+        const {id}=event.data;
+        await User.deleteOne({clerkId:id});
+        //await deleteStreamUser(id.toString());
+    }
+);
 
 
 // Create an empty array where we'll export future Inngest functions
-export const functions = [];
+export const functions = [syncUser,deleteUserFromDB];
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,13 +2,15 @@ import express from "express";
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 import {clerkMiddleware} from "@clerk/express";
-
+import { functions, inngest } from "./config/innjest.js";
+import {serve} from "inngest/express";
 
 const app=express();
 
+app.use(express.json());
 app.use(clerkMiddleware());
 
-
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 
 app.get("/",(req,res)=>{
@@ -17,7 +19,23 @@ app.get("/",(req,res)=>{
 
 
 
-app.listen(ENV.PORT,()=>{
-    console.log("Server is running on port",ENV.PORT);
-    connectDB()
-})
+const startServer=async()=>{
+    try{
+            await connectDB();
+            if(ENV.NODE_ENV!=="production"){
+                app.listen(ENV.PORT,()=>{
+                    console.log("Server started on port",(ENV.PORT));
+                });
+            }     
+    }
+    catch(error)
+    {
+        console.error("Error starting server ",error);
+        process.exit(1);
+    }
+}
+
+/*app.listen(ENV.PORT,()=>{
+                    console.log("Server started on port",(ENV.PORT));
+                    connectDB();
+                });*/
