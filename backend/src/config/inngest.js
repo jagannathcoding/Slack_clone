f698c71bce34447bcf599437b4c0555f68a804a4
jagannathcoding <jagannathdebgunin@gmail.com>
2025-09-30 00:18:19 +0530

    emni

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -75,7 +75,7 @@ import { connectDB } from "./db.js";
 import { User } from "../models/user.model.js"; // Import the User model
 import {  deleteStreamUser, upsertStreamUser } from "./stream.js";
 
-import { upsertStreamUser } from "./stream.js";
+//import { upsertStreamUser } from "./stream.js";
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "slack-clone" });
 
