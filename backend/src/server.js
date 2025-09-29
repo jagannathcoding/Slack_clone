3cdc4f8663add6fa06ccfd0b93929579a2ff26ce
jagannathcoding <jagannathdebgunin@gmail.com>
2025-09-30 00:11:16 +0530

    sever

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,9 @@
-/*import "../instrument.mjs";
 import express from "express";
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 import {clerkMiddleware} from "@clerk/express";
 import { functions, inngest } from "./config/inngest.js";
 import {serve} from "inngest/express";
-import chatRoutes from "./routes/chat.route.js";
-
-import * as Sentry from "@sentry/node";
-
 
 const app=express();
 
@@ -17,19 +12,12 @@ app.use(clerkMiddleware());
 
 
 
-app.get("/debug-sentry",(req,res)=>{
-    throw new Error("My first sentry error");
-});
-
-
 app.get("/",(req,res)=>{
     res.send("Hello WOrld 123");
 });
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
-app.use("/api/chat",chatRoutes);
 
-Sentry.setupExpressErrorHandler(app);
 
 
 const startServer=async()=>{
@@ -46,50 +34,6 @@ const startServer=async()=>{
         console.error("Error starting server ",error);
         process.exit(1);
     }
-};
-
-startServer();
-
-export default app;
-
-*/
-
-import express from "express";
-import { ENV } from "./config/env.js";
-import { connectDB } from "./config/db.js";
-import {clerkMiddleware} from "@clerk/express";
-import { functions, inngest } from "./config/inngest.js";
-import {serve} from "inngest/express";
-
-const app=express();
-
-app.use(express.json());
-app.use(clerkMiddleware());
-
-
-
-app.get("/",(req,res)=>{
-res.send("Hello WOrld 123");
-});
-
-app.use("/api/inngest", serve({ client: inngest, functions }));
-
-
-
-const startServer=async()=>{
-try{
-await connectDB();
-if(ENV.NODE_ENV!=="production"){
-app.listen(ENV.PORT,()=>{
-console.log("Server started on port",(ENV.PORT));
-});
-}     
-}
-catch(error)
-{
-console.error("Error starting server ",error);
-process.exit(1);
-}
 };
 
 startServer();
