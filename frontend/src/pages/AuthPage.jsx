98a6e89ed8121cca465600d9f637446aa8edb209
jagannathcoding <jagannathdebgunin@gmail.com>
2025-10-01 22:03:14 +0530

    Working in authpage

--- frontend/src/pages/AuthPage.jsx
+++ frontend/src/pages/AuthPage.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const AuthPage = () => {
   return (
-    <div>AuthPage</div>
+    <div className='auth-container'>
+      <div className="auth-left">
+        <div className="auth-hero">
+          <div className="brand container">
+            <img src="/logo.png" className='brand-logo'/>
+            <span className="brand-name">EMni</span>
+          </div>
+
+          <h1 className="hero-title">
+            Welcome to work
+          </h1>
+        </div>
+      </div>
+    </div>
   )
 }
 
