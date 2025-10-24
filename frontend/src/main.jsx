c43bfe27900104c11a4b053cc41ed3f0d4fa4640
jagannathcoding <jagannathdebgunin@gmail.com>
2025-10-24 16:53:29 +0530

    Added auth provider and tools setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,18 @@
 import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import{ClerkProvider} from '@clerk/clerk-react'
-import {BrowserRouter} from 'react-router';
+import {
+  Routes,
+  Route,
+  BrowserRouter,
+  useLocation,
+  useNavigationType,
+  createRoutesFromChildren,
+  matchRoutes,
+} from "react-router";
 
 import {
   //useQuery,
@@ -14,6 +23,9 @@ import {
 } from '@tanstack/react-query'
 
 import {Toaster} from 'react-hot-toast';
+import AuthProvider from './providers/AuthProvider.jsx'
+
+import * as Sentry from "@sentry/react";
 
 const queryClient=new QueryClient();
 
@@ -26,13 +38,34 @@ if (!PUBLISHABLE_KEY) {
 }
 
 
+Sentry.init({
+  dsn: "https://5f15d463c524197e4ddb0c34dbc5a971@o4510009836240898.ingest.us.sentry.io/4510244145332224",
+  integrations: [
+    Sentry.reactRouterV7BrowserTracingIntegration({
+      useEffect: React.useEffect,
+      useLocation,
+      useNavigationType,
+      createRoutesFromChildren,
+      matchRoutes,
+    }),
+  ],
+  tracesSampleRate: 1.0,
+});
+
+
+
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <BrowserRouter>
 
       <QueryClientProvider client={queryClient}>
+
+        <AuthProvider>
       <App />
+      </AuthProvider>
       <Toaster position="top-right"/>
       </QueryClientProvider>
       </BrowserRouter>  
