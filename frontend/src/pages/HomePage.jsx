d623e04213d97ea403212970b09c67010d07d08d
jagannathcoding <jagannathdebgunin@gmail.com>
2026-03-08 00:02:32 +0530

    Made corrections in server.js in backend and added channel in homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,30 @@
 import { UserButton } from "@clerk/clerk-react"
-
+import{useSearchParams} from "react-router"
+import { useStreamChat } from "../hooks/useStreamChat";
+import { useEffect, useState } from "react";
+import PageLoader from "../components/PageLoader";
 
 const HomePage = () => {
 
   const [isCreateModalOpen, setIsCreateModalOpen] = useState(false);
   const[activeChannel, setActiveChannel]= useState(null);
-  return (
+  const [searchParams, setSearchParams] = useSearchParams();
+ 
+  const{chatClient,error,isLoading}=useStreamChat();
+  useEffect(()=>{
+    if(chatClient){
+      const channelId=searchParams.get("channel");
+      if(channelId){
+        const channel=chatClient.channel("mesaging",channelId);
+        setActiveChannel(channel);
+      }
+    }
+  },[chatClient,searchParams]);
+
+  if(error) return<p>Something went wrong...</p>
+  if(isLoading||!chatClient) return<PageLoader/>
+
+   return (
     <div className>
       <UserButton/>
       Home Page
